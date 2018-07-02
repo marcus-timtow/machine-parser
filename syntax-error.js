0c739a3bea3569629e5f9fb79254e8a34508e238
downtempo <17656228+marcus-timtow@users.noreply.github.com>
2018-07-02 14:52:16 +0200

    Update syntax-error.js

--- syntax-error.js
+++ syntax-error.js
@@ -1,53 +1,72 @@
-
-/**
- * A syntax error which may be encountered while parsing a configuration file.
- * 
- * @constructor
- * @extends Error
- * 
- * @argument {string} msg
- * @argument {object} details
- * @argument {string} [details.file] file name
- * @argument {number} [details.linenumber] line number
- * @argument {string} [details.line] line
- * @argument {number} [details.charnumber] charnumber
- * @argument {string} [details.section] part of the line containing a syntax error
- */
-let SyntaxError = function (msg, details) {
-    //Error.call(this, msg);
-    this.message = msg;
-    details = details || {};
-    this.linenumber = details.linenumber;
-    this.line = details.line;
-    this.charnumber = details.charnumber;
-    if (typeof this.charnumber=== "number" && typeof this.line === "string" && this.line.length >= this.charnumber){
-        this.char = this.line.charAt(this.charnumber -1);
+(function (deps, definition) {
+    if (!definition) {
+        definition = deps;
+        deps = [];
     }
-    this.section = details.section;
-    this.file = details.file;
-};
-//SyntaxError.prototype = Object.create(Error.prototype);
-SyntaxError.prototype.toJSON = function () {
-    let ret = {
-        message: this.message
-    };
-    for (let prop of ["linenumber", "line", "section", "file", "charnumber", "char"]) {
-        if (this.hasOwnProperty(prop) && this[prop] !== undefined) {
-            ret[prop] = this[prop];
+    if (!Array.isArray(deps)) {
+        deps = [deps];
+    }
+    if (typeof define === "function" && typeof define.amd === "object") {
+        define(deps, definition);
+    } else if (typeof module !== "undefined") {
+        module.exports = definition.apply(this, deps.map(function (dep) {
+            return require(dep);
+        }));
+    } else {
+        throw new Error("missing module loader");
+    }
+})(function () {
+
+
+    /**
+     * A syntax error which may be encountered while parsing a configuration file.
+     * 
+     * @constructor
+     * @extends Error
+     * 
+     * @argument {string} msg
+     * @argument {object} details
+     * @argument {string} [details.file] file name
+     * @argument {number} [details.linenumber] line number
+     * @argument {string} [details.line] line
+     * @argument {number} [details.charnumber] charnumber
+     * @argument {string} [details.section] part of the line containing a syntax error
+     */
+    let SyntaxError = function (msg, details) {
+        //Error.call(this, msg);
+        this.message = msg;
+        details = details || {};
+        this.linenumber = details.linenumber;
+        this.line = details.line;
+        this.charnumber = details.charnumber;
+        if (typeof this.charnumber === "number" && typeof this.line === "string" && this.line.length >= this.charnumber) {
+            this.char = this.line.charAt(this.charnumber - 1);
         }
-    }
-    return ret;
-};
-SyntaxError.prototype.toString = function () {
-    let msg = "file " + (this.file || "-") + ": line " + (this.linenumber !== undefined ? this.linenumber : "-") + ": char " + (this.charnumber !== undefined ? (this.charnumber + ' "' + this.char + '"') : "-") + ": " + this.message;
-    if (this.hasOwnProperty("line") && this.line) {
-        msg += "\n>  " + this.line;
-    }
-    if (this.hasOwnProperty("section") && this.section) {
-        msg += "\n>> " + this.section;
-    }
-    return msg;
-};
+        this.section = details.section;
+        this.file = details.file;
+    };
+//SyntaxError.prototype = Object.create(Error.prototype);
+    SyntaxError.prototype.toJSON = function () {
+        let ret = {
+            message: this.message
+        };
+        for (let prop of ["linenumber", "line", "section", "file", "charnumber", "char"]) {
+            if (this.hasOwnProperty(prop) && this[prop] !== undefined) {
+                ret[prop] = this[prop];
+            }
+        }
+        return ret;
+    };
+    SyntaxError.prototype.toString = function () {
+        let msg = "file " + (this.file || "-") + ": line " + (this.linenumber !== undefined ? this.linenumber : "-") + ": char " + (this.charnumber !== undefined ? (this.charnumber + ' "' + this.char + '"') : "-") + ": " + this.message;
+        if (this.hasOwnProperty("line") && this.line) {
+            msg += "\n>  " + this.line;
+        }
+        if (this.hasOwnProperty("section") && this.section) {
+            msg += "\n>> " + this.section;
+        }
+        return msg;
+    };
 
-module.exports = SyntaxError;
-
+    return SyntaxError;
+});
