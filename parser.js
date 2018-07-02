9d91659a4caed20e2466749da0b6d986b72f936c
downtempo <17656228+marcus-timtow@users.noreply.github.com>
2018-07-02 14:53:13 +0200

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,43 +1,57 @@
+(function (deps, definition) {
+    if (!definition) {
+        definition = deps;
+        deps = [];
+    }
+    if (!Array.isArray(deps)) {
+        deps = [deps];
+    }
+    if (typeof define === "function" && typeof define.amd === "object") {
+        define(deps, definition);
+    } else if (typeof module !== "undefined") {
+        module.exports = definition.apply(this, deps.map(function (dep) {
+            return require(dep);
+        }));
+    } else {
+        throw new Error("missing module loader");
+    }
+})(["./syntax-error", "./preparse", "./machine", "./defaults"], function (SyntaxError, preparse, Factory, defaultMachines) {
 
-var SyntaxError = require("./syntax-error");
-var preparse = require("./preparse");
-var Factory = require("./machine");
+    return function () {
+        var factory = new Factory();
 
-var Parser = function () {
-    var factory = new Factory();
+        var parse = function (type, line) {
+            if (typeof line === "string") {
+                line = {
+                    line: line
+                };
+            }
 
-    var parse = function (type, line) {
-        if (typeof line === "string") {
-            line = {
-                line: line
-            };
-        }
-
-        let machine = factory.make(type);
-        for (let i = 0; i < line.line.length; i++) {
-            let char = line.line.charAt(i);
-            if (machine.test(char)) {
-                machine.push(char);
-            } else {
-                throw new SyntaxError("machine " + type + ": unexpected character",
-                        {file: line.original_file, linenumber: line.original_line_number, line: line.original_line || line.line, section: char, charnumber: i + 1});
+            let machine = new (factory.get(type))();
+            for (let i = 0; i < line.line.length; i++) {
+                let char = line.line.charAt(i);
+                if (machine.test(char)) {
+                    machine.push(char);
+                } else {
+                    throw new SyntaxError("machine " + type + ": unexpected character",
+                            {file: line.original_file, linenumber: line.original_line_number, line: line.original_line || line.line, section: char, charnumber: i + 1});
+                }
+            }
+            if (!machine.complete) {
+                throw new SyntaxError("machine " + type + ": unexpected EOL",
+                        {file: line.original_file, linenumber: line.original_line_number, line: line.original_line || line.line});
+            }
+            return machine.parse();
+        };
+        parse.preparse = preparse;
+        parse.SyntaxError = SyntaxError;
+        for (let prop in factory) {
+            if (typeof factory[prop] === "function") {
+                parse[prop] = factory[prop].bind(factory);
             }
         }
-        if (!machine.complete) {
-            throw new SyntaxError("machine " + type + ": unexpected EOL",
-                    {file: line.original_file, linenumber: line.original_line_number, line: line.original_line || line.line});
-        }
-        return machine.parse();
+        defaultMachines.registerAt(parse);
+
+        return parse;
     };
-    parse.preparse = preparse;
-    parse.SyntaxError = SyntaxError;
-    parse.register = factory.register.bind(factory);
-    parse.registerChar = factory.registerChar.bind(factory);
-    parse.registerString = factory.registerString.bind(factory);
-    parse.alias = factory.alias.bind(factory);
-
-    return parse;
-};
-
-module.exports = Parser;
-
+});
